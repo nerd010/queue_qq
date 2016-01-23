5460884367b7bf3a6e33fa1fff9afe13225827d9
王增康 <Charles@wangzengkangdemacbook-pro.local>
2016-01-23 20:36:54 +0800

    Initial Commit

---
+++ Queue_qq号/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Queue_qq号
+//
+//  Created by 王增康 on 16/1/23.
+//  Copyright © 2016年 CHW. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
