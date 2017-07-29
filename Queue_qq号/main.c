9c1b32d892fa71124b798f3188b750d15eff0100
smallX <xieyu559@163.com>
2017-07-29 12:31:55 +0800

    add files

--- Queue_qq号/main.c
+++ Queue_qq号/main.c
@@ -8,8 +8,22 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main() {
+    int q[102] = {0,6,3,1,7,5,8,9,2,4},head,tail;
+    head = 1;
+    tail = 10;
+    while (head < tail)
+    {
+        printf("%d",q[head]);
+        head++;
+        
+        q[tail] = q[head];
+        tail++;
+        head++;
+    }
+    
+    getchar();getchar();
+    
+    
     return 0;
 }
